1757cac2bcc59603fbc9c2bfcd94fb35a155a054
dandodado <jo4326892@gmail.com>
2025-12-05 18:33:35 +0900

    add attractor

---
+++ CirclePower.js
@@ -0,0 +1,35 @@
+class CirclePower {
+
+  constructor() {
+    this.position = createVector(width /2, height / 2);
+    this.mass = 20;
+    this.dragOffset = createVector(0, 0);
+  }
+
+  getForce(m) {
+    let force = p5.Vector.sub(this.position, m.position);
+    let distance = force.mag();
+    distance = constrain(distance, 5, 25);
+    force.normalize();
+    let strength = this.mass / (distance * distance);
+    force.mult(strength);
+    return force;
+  }
+
+  // Method to display
+  show() {
+    ellipseMode(CENTER);
+    strokeWeight(4);
+    stroke(0);
+    if (this.dragging) {
+      fill(255);
+    } else if (this.rollover) {
+      fill(175);
+    } else {
+      fill(101, 200);
+    }
+    ellipse(this.position.x, this.position.y, this.mass * 2, this.mass * 2);
+  }
+}
+ 
+
--- sketch.js
+++ sketch.js
@@ -1,16 +1,17 @@
 let emitter;
-
+let attractor;
 function setup() {
   createCanvas(800 , 800);
   emitter = new Emitter(width / 2, height / 2);
-
+  attractor = new CirclePower();
 }
 
 function draw() {
   background(200);
   emitter.addParticle();
-  
   emitter.run();
+  attractor.show();
+  emitter.applyForceObject(attractor);
 
 
 
